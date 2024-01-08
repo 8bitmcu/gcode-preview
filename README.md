ee073a8bb7bd8d22d9aefdb5227f9cd6d51a0f31
Vincent <agashka2@gmail.com>
2024-01-08 10:56:30 -0400

    cleanup code; added bash version

---
+++ README.md
@@ -0,0 +1,18 @@
+gcode-preview
+=============
+
+Extract images as png/jpg from .gcode files with C or Bash
+
+
+Building and installing the C version
+-------------------------------------
+
+1. clone this repository locally on your machine
+2. run `make clean && sudo make install` from within the repository folder
+
+
+Which is faster?
+----------------
+
+
+
--- main.c
+++ main.c
@@ -1,14 +1,12 @@
+#include "Turbo-Base64/turbob64.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "Turbo-Base64/turbob64.h"
 
 static const char thumb_begin[] = "; thumbnail begin";
 static const char thumb_end[] = "; thumbnail end\n";
 
-void 
-gcodeimg_to_file(char* input, char* output)
-{
+void gcodeimg_to_file(char *input, char *output) {
   // open input file for reading
   FILE *fp = fopen(input, "r");
   if (fp == NULL) {
@@ -29,7 +27,8 @@ gcodeimg_to_file(char* input, char* output)
 
     // skip everything until we find the magic string
     if (thumbnail_begin == 0) {
-      thumbnail_begin = strncmp(line, thumb_begin, sizeof(thumb_begin)-1) == 0;
+      thumbnail_begin =
+          strncmp(line, thumb_begin, sizeof(thumb_begin) - 1) == 0;
       continue;
     }
 
@@ -38,13 +37,13 @@ gcodeimg_to_file(char* input, char* output)
       break;
 
     // copy base64 string into b64
-    b64 = realloc(b64, (total_len+line_len) * sizeof(unsigned char)); 
+    b64 = realloc(b64, (total_len + line_len) * sizeof(unsigned char));
     memcpy(b64 + total_len, &line[2], line_len);
     total_len += line_len;
   }
 
   // allocate 3/4th the memory for the output
-  unsigned char out[(total_len*3/4)+8];
+  unsigned char out[(total_len * 3 / 4) + 8];
 
   // decode b64 to out
   tb64xdec(b64, total_len, out);
@@ -61,35 +60,28 @@ gcodeimg_to_file(char* input, char* output)
     free(b64);
 }
 
-static void
-usage(void)
-{
-	puts("usage: gcode-preview [-i inputfile] [-o outputfile]");
+static void usage(void) {
+  puts("usage: gcode-preview [-i inputfile] [-o outputfile]");
 }
 
-
-int
-main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   int i;
-  char* input;
-  char* output;
+  char *input;
+  char *output;
   for (i = 1; i < argc; i++)
-		/* these options take no arguments */
-		if (!strcmp(argv[i], "-v")) {      /* prints version information */
-			puts("gcode-preview-0.1");
-			exit(EXIT_SUCCESS);
-    }
-  else if (!strcmp(argv[i], "-h")) {      /* prints version information */
-			usage();
-			exit(EXIT_SUCCESS);
+    /* these options take no arguments */
+    if (!strcmp(argv[i], "-v")) { /* prints version information */
+      puts("gcode-preview-0.1");
+      exit(EXIT_SUCCESS);
+    } else if (!strcmp(argv[i], "-h")) { /* prints version information */
+      usage();
+      exit(EXIT_SUCCESS);
     }
     /* these options take one argument */
-		else if (!strcmp(argv[i], "-i"))   /* input filename */
-			input = argv[++i];
-		else if (!strcmp(argv[i], "-o"))   /* input filename */
-			output = argv[++i];
-
+    else if (!strcmp(argv[i], "-i")) /* input filename */
+      input = argv[++i];
+    else if (!strcmp(argv[i], "-o")) /* input filename */
+      output = argv[++i];
 
   if (!input) {
     puts("input file not specified");
